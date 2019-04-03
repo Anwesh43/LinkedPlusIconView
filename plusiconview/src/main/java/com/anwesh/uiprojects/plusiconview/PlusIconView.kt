e47334e8c115f94f5989dc4a16cebe69857f17b9
Anwesh43 <anweshthecool0@gmail.com>
2019-04-03 07:04:22 +0530

    adding libraries and constants

--- plusiconview/src/main/java/com/anwesh/uiprojects/plusiconview/PlusIconView.kt
+++ plusiconview/src/main/java/com/anwesh/uiprojects/plusiconview/PlusIconView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.plusiconview
 /**
  * Created by anweshmishra on 03/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#BDBDBD")
