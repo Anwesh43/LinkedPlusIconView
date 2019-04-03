c5aa109eeb0b48dbba7017cd4b21e25c78ad4ca3
Anwesh43 <anweshthecool0@gmail.com>
2019-04-03 07:16:42 +0530

    adding drawPINode canvas extension method

--- plusiconview/src/main/java/com/anwesh/uiprojects/plusiconview/PlusIconView.kt
+++ plusiconview/src/main/java/com/anwesh/uiprojects/plusiconview/PlusIconView.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#1565C0")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val plusSizeFactor : Float = 1.6f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -28,3 +29,27 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawPINode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    val size : Float = gap / sizeFactor
+    val plusSize : Float = size / plusSizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawArc(RectF(-size, -size, size, size), 0f, 360f * sc1, true, paint)
+    for (j in 0..(lines - 1)) {
+        paint.color = backColor 
+        save()
+        rotate(90f * j)
+        drawLine(0f, 0f, plusSize * sc2.divideScale(j, lines), 0f, paint)
+        restore()
+    }
+    restore()
+}
