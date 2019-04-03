ccde2beebd5aee43af5ddc1e0fcbaf9c4d8fe6c6
Anwesh43 <anweshthecool0@gmail.com>
2019-04-03 07:37:54 +0530

    adding PlusIcon class

--- plusiconview/src/main/java/com/anwesh/uiprojects/plusiconview/PlusIconView.kt
+++ plusiconview/src/main/java/com/anwesh/uiprojects/plusiconview/PlusIconView.kt
@@ -159,7 +159,29 @@ class PlusIconView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class PlusIcon(var i : Int) {
+        private var curr : PINode = PINode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
