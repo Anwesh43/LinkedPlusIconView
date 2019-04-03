b409a469ba9fca3352fd4ebc5bf64c87aa984e42
Anwesh43 <anweshthecool0@gmail.com>
2019-04-03 07:57:21 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedplusiconview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedplusiconview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedplusiconview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.plusiconview.PlusIconView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PlusIconView.create(this)
     }
 }
